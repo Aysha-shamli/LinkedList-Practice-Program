5ff11184e0c8477e467564551de7c5882ba2865a
Aysha-shamli <ayshashamli1999@gmail.com>
2023-11-20 19:02:45 +0530

    added Node.java and Simplelinkedlist programs

---
+++ SimpleLinkedList.java
@@ -0,0 +1,40 @@
+package linkedListPrograms;
+
+public class SimpleLinkedList {
+    Node head;
+    public void addNode(int data){
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+
+    public void printData(){
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.addNode(56);
+        list.addNode(30);
+        list.addNode(70);
+        list.printData();
+    }
+
+}
